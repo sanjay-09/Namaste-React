df27e0a0091ec88e9758809cae7d497c7ce94893
Sanjay Singh <63473096+sanjay-09@users.noreply.github.com>
2023-09-10 12:28:34 +0530

    episode 2

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
 const parent = React.createElement(
   "div",
   { id: "parent" },
-  React.createElement(
+  React.createElement(  
     "div",
     { id: "child" },
     React.createElement("h1", {}, "I am tag h1")
